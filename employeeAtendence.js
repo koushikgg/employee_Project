422d440315c35b6980cc8598db5e6857b3b02236
koushikgg <iamkoushikg@gmail.com>
2024-05-29 18:50:12 +0530

    Employee Atendece Project

---
+++ employeeAtendence.js
@@ -0,0 +1,38 @@
+const readline = require("readline-sync");
+employeeList=[
+    {
+        emp_id:1000,
+        name:"virat",
+        department:"ECE",
+        atendence : "Present",
+        wage:0
+    },
+    {
+        emp_id:1001,
+        name:"abd",
+        department:"CSE",
+        atendence : "Present",
+        wage:0
+    },
+    {
+        emp_id:1002,
+        name:"gayle",
+        department:"ISE",
+        atendence : "Present",
+        wage:0
+    },
+    {
+        emp_id:1003,
+        name:"sami",
+        department:"ECE",
+        atendence : "Present",
+        wage:0
+    },
+    {
+        emp_id:1004,
+        name:"bhuvi",
+        department:"CSE",
+        atendence : "Present",
+        wage:0
+    }
+]
