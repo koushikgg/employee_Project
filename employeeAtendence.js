5dadeb504e1cae3f43351a1ab450c5e04f7a6bf7
koushikgg <iamkoushikg@gmail.com>
2024-05-30 09:24:46 +0530

    UC3 added to employeeAtendence.js

--- employeeAtendence.js
+++ employeeAtendence.js
@@ -4,40 +4,44 @@ employeeList=[
         name:"virat",
         department:"ECE",
         atendence : "Present",
-        wage:0
+        wage:0,
+        atendenceData:[]
     },
     {
         emp_id:1001,
         name:"abd",
         department:"CSE",
         atendence : "Present",
-        wage:0
+        wage:0,
+        atendenceData:[]
     },
     {
         emp_id:1002,
         name:"gayle",
         department:"ISE",
         atendence : "Present",
-        wage:0
+        wage:0,
+        atendenceData:[]
     },
     {
         emp_id:1003,
         name:"sami",
         department:"ECE",
         atendence : "Present",
-        wage:0
+        wage:0,
+        atendenceData:[]
     },
     {
         emp_id:1004,
         name:"bhuvi",
         department:"CSE",
         atendence : "Present",
-        wage:0
+        wage:0,
+        atendenceData:[]
     }
 ]
-const fullDay=8;
-const halfDay=4;
-const a= []
+
+
  function genrateAtendence(){
         for (let value of employeeList){
             let attend = Math.floor(Math.random()*2)
@@ -51,7 +55,12 @@ const a= []
 function addWage(){
     for (let value of employeeList){
         if (value.atendence=="Present"){
-            value.wage=8*20
+            let attend = Math.floor(Math.random()*2)
+            if (attend==0){
+                value.wage=4*20
+            }else{
+                value.wage=8*20
+            }
         }
     }
 }
